cd2cdcdd39bb1c8a74f9a9134c5d577e486edd0c
Matheus Severo Goerg <matheusseverogoerg.jobs@gmail.com>
2021-08-25 10:57:06 -0300

    Envio da calculadora de IMC com correções para o Scanner

--- src/revisao/IMC.java
+++ src/revisao/IMC.java
@@ -15,51 +15,62 @@ import java.util.Scanner;
  */
 public class IMC {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static String leTexto(){
+        Scanner teclado =  new Scanner(System.in);
+        return teclado.nextLine();
+    }
+    
+    public static String lePalavra(){
+        Scanner teclado = new Scanner(System.in);
+        return teclado.next();
+    }
+    
     public static void main(String[] args) {
+        
         // TODO code application logic here
         Scanner ler = new Scanner(System.in);
         float peso, altura, imc;
-        String nome;
+        String nome, saida = "s";
+        boolean s = true;
         
-        System.out.println("----- Calculadora IMC -----");
-        System.out.println("");
-        System.out.print("Informe seu nome: ");
-        //ler.nextLine();
-        nome = ler.nextLine();
-        System.out.println("");
-        System.out.print("Informe seu peso: ");
-        peso = ler.nextFloat();
-        System.out.println("");
-        System.out.print("Informe sua altura: ");
-        altura = ler.nextFloat();
-        imc = peso / (altura * altura);
+        while (s) {
         
-        
-        System.out.println("");
-        System.out.println("Resultado com IF com formatação na saída");
-        if (imc < 18.5) {
-            System.out.printf(nome + " seu IMC é: %.2f, e define Magreza.", imc);
-        } else if (imc <= 24.9) {
-            System.out.printf(nome + " seu IMC é: %.2f, e define Normal.", imc);
-        } else if (imc <= 30) {
-            System.out.printf(nome + " seu IMC é: %.2f, e define Sobrepeso.", imc);
-        } else {
-            System.out.printf(nome + " seu IMC é: %.2f, e define Obesidade.", imc);
+            System.out.println("----- Calculadora IMC -----");
+            System.out.print("\nInforme seu nome: ");
+            nome = leTexto();
+            System.out.print("\nInforme seu peso: ");
+            peso = ler.nextFloat();
+            System.out.print("\nInforme sua altura: ");
+            altura = ler.nextFloat();
+            imc = peso / (altura * altura);
+
+
+            System.out.println("");
+            System.out.println("Resultado com IF com formatação na saída");
+            if (imc < 18.5) {
+                System.out.printf(nome + " seu IMC é: %.2f, e define Magreza.", imc);
+            } else if (imc <= 24.9) {
+                System.out.printf(nome + " seu IMC é: %.2f, e define Normal.", imc);
+            } else if (imc <= 30) {
+                System.out.printf(nome + " seu IMC é: %.2f, e define Sobrepeso.", imc);
+            } else {
+                System.out.printf(nome + " seu IMC é: %.2f, e define Obesidade.", imc);
+            }
+
+            System.out.println("\n\nNumberFormat");
+            NumberFormat nf = NumberFormat.getInstance();
+            nf.setMaximumFractionDigits(2);
+            System.out.println(nf.format(imc));
+
+            System.out.println("\nDecimalFormat");
+            DecimalFormat df = new DecimalFormat("#,###.00");
+            System.out.println(df.format(imc));
+            
+            System.out.println("\nVocê deseja calcular o IMC novamente?\n(s) para continuar: ");
+            saida = lePalavra();
+            s = saida.equalsIgnoreCase("s");
+            
         }
-        System.out.println("");
-        
-        System.out.println("");
-        System.out.println("NumberFormat");
-        NumberFormat nf = NumberFormat.getInstance();
-        nf.setMaximumFractionDigits(2);
-        System.out.println(nf.format(imc));
-        
-        System.out.println("");
-        System.out.println("DecimalFormat");
-        DecimalFormat df = new DecimalFormat("#,###.00");
-        System.out.println(df.format(imc));
    } 
 }
